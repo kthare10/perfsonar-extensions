5e527720726c9a32b3e3ae444f5c669f90f2c518
Komal Thareja <kthare10@renci.org>
2025-03-21 17:16:29 -0400

    custom cron

---
+++ custom-cron.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Read environment variables
+HOSTS="${HOSTS}"
+INTERVAL="${INTERVAL:-0 */2 * * *}"
+
+if [ -z "$HOSTS" ]; then
+  echo "No hosts provided! Exiting."
+  exit 1
+fi
+
+echo "${INTERVAL} /usr/bin/python3 /usr/src/app/periodic.py --hosts ${HOSTS} --output-dir /data >> /data/cron.log 2>&1" > /etc/cron.d/daily-tests
+chmod 0644 /etc/cron.d/daily-tests
+crontab /etc/cron.d/daily-tests
+
+echo "Cron job added for hosts: ${HOSTS} at interval: ${INTERVAL}"
+
+/usr/sbin/cron -f
